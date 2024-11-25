4d7743e3fd82fac3aef4a84de129e2b30f69131e
stas420 <sniemczewski@onet.pl>
2024-11-25 22:50:17 +0100

    Managed to add nlohmann/json external library, 99% working

--- src/2ects_project/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/qtcsettings.cmake
+++ src/2ects_project/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/qtcsettings.cmake
@@ -1,10 +1,2 @@
 # This file is managed by Qt Creator, do not edit!
 
-set("CMAKE_CXX_COMPILER" "E:/qt/Qt/Tools/mingw1310_64/bin/g++.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_BUILD_TYPE" "Debug" CACHE "STRING" "" FORCE)
-set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
-set("CMAKE_C_COMPILER" "E:/qt/Qt/Tools/mingw1310_64/bin/gcc.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "E:/qt/Qt/6.8.0/mingw_64" CACHE "PATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "E:/qt/Qt/6.8.0/mingw_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PROJECT_INCLUDE_BEFORE" "D:/projekt IO/2ects_io_project/src/2ects_project/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_FLAGS_INIT" "-DQT_QML_DEBUG" CACHE "STRING" "" FORCE)
--- src/2ects_project/main.cpp
+++ src/2ects_project/main.cpp
@@ -1,9 +1,17 @@
 #include "mainwindow.h"
-
+#include "extlibs/nlohmann/json.hpp" // < will underline a warning like "header is not used directly - well, if it builds, leave it that way
 #include <QApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
+    try {
+        nlohmann::json json = nlohmann::json::parse("");
+    }
+    catch (...) {
+        std::cout << "parsing did not work :(" << std::endl;
+    }
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
