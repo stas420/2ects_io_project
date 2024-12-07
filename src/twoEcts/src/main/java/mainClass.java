29e6c91735d7c01851e14741ccf025dcaa08da01
stas420 <sniemczewski@onet.pl>
2024-12-07 13:13:10 +0100

    tryna add tesseract wrapper, but not working :(

--- src/twoEcts/src/main/java/mainClass.java
+++ src/twoEcts/src/main/java/mainClass.java
@@ -1,5 +1,21 @@
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import java.io.File;
+
 public class mainClass {
     public static void main(String[] args) {
+
+        File img = new File("/src/files/text.jpg");
+        ITesseract instance = new Tesseract();
+
+        try {
+            String text = instance.doOCR(img);
+            System.out.println(text);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
         System.out.println("2ects");
     }
 }
